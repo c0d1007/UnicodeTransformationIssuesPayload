f5a357f410152f0d3ba14f3e7e9a7bad5eeb0930
c0d1007 <43197505+c0d1007@users.noreply.github.com>
2019-04-21 14:25:12 +0800

    Update wvs_unicode_generate.py

--- wvs_unicode_generate.py
+++ wvs_unicode_generate.py
@@ -1,11 +1,6 @@
 def unicode_encode(s):
     result = ''
     for c in s:
-        '''
-        print(urllib.quote(c))
-        print(hex(ord(c)))
-        print(hex(ord(c) + 0x80))
-        '''
         if 48 <= ord(c) <= 57 or 65 <= ord(c) <= 90 or 97 <= ord(c) <= 122:
             result += c
         else:
